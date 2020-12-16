a13e3b14ae1d69a888651b9e5187b7eafc5d3ff7
matura1999 <yipengpeter@gmail.com>
2020-12-16 17:52:03 +1100

    fetch data from backend

--- src/pages/UserCentre/components/Content/components/PersonalInfo/PersonalInfo.js
+++ src/pages/UserCentre/components/Content/components/PersonalInfo/PersonalInfo.js
@@ -8,21 +8,64 @@ import {
   Title,
   Submit,
 } from "../styling";
+import getAuth from "../../../../../../apis/getAuth";
 
+const TOKEN = localStorage.getItem("JWT_TOKEN");
 class PersonalInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "Yipeng",
-      lastName: "Wang",
-      age: 21,
-      phone: "0479088345",
-      email: "yipwang@unimelb.student.edu.au",
-      healthCondition: "asndajsdnasdkalsdnaldn",
+      loading: true,
+      data: undefined,
+      role: undefined,
+      // firstName: "Yipeng",
+      // lastName: "Wang",
+      // age: 21,
+      // phone: "0479088345",
+      // email: "yipwang@unimelb.student.edu.au",
+      // healthCondition: "asndajsdnasdkalsdnaldn",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    getAuth({ token: TOKEN })
+      .then((response) => {
+        const { accountId, grantedAuthorities } = response.data;
+        const { authority } = grantedAuthorities[0];
+        const role = authority === "ROLE_DOCTOR" ? "doctors" : "patients";
+        const url = `http://localhost:8080/${role}/search?accountId=${accountId}`;
+        this.setState({
+          role,
+        });
+        this.getPersonalInfo(url).then((data) => {
+          this.setState({
+            data,
+            loading: false,
+          });
+        });
+      })
+      .catch(() => {});
+  }
+
+  async getPersonalInfo(url) {
+    const request = {
+      mode: "cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    return await fetch(url, request)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   handleChange(event) {
@@ -34,41 +77,50 @@ class PersonalInfo extends React.Component {
   }
 
   render() {
+    const { loading, data, role } = this.state;
+
+    console.log(data, role);
     return (
-      <Layout onSubmit={this.handleSubmit}>
-        <Title>Personal Information</Title>
-        <FormItem>
-          <Label>First Name:</Label>
-          <FormInput type="text" defaultValue={this.state.firstName} />
-        </FormItem>
-        <FormItem>
-          <Label>Last Name:</Label>
-          <FormInput type="text" defaultValue={this.state.lastName} />
-        </FormItem>
-        <FormItem>
-          <Label>Age:</Label>
-          <FormInput type="int" defaultValue={this.state.age} />
-        </FormItem>
-        <FormItem>
-          <Label>Phone:</Label>
-          <FormInput type="text" defaultValue={this.state.phone} />
-        </FormItem>
-        <FormItem>
-          <Label>Email:</Label>
-          <FormInput type="text" defaultValue={this.state.email} />
-        </FormItem>
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <Layout onSubmit={this.handleSubmit}>
+            <Title>Personal Information</Title>
+            <FormItem>
+              <Label>First Name:</Label>
+              <FormInput type="text" defaultValue={data.firstName} />
+            </FormItem>
+            <FormItem>
+              <Label>Last Name:</Label>
+              <FormInput type="text" defaultValue={data.lastName} />
+            </FormItem>
+            <FormItem>
+              <Label>Age:</Label>
+              <FormInput type="int" defaultValue={data.age} />
+            </FormItem>
+            <FormItem>
+              <Label>Phone:</Label>
+              <FormInput type="text" defaultValue={data.phone} />
+            </FormItem>
+            <FormItem>
+              <Label>Email:</Label>
+              <FormInput type="text" defaultValue={data.email} />
+            </FormItem>
 
-        <FormItem healthCondition>
-          <Label>Health Condition:</Label>
+            <FormItem healthCondition>
+              <Label>Health Condition:</Label>
 
-          <HealthCondition
-            defaultValue={this.state.healthCondition}
-            onChange={this.handleChange}
-          />
-        </FormItem>
+              <HealthCondition
+                defaultValue={data.healthCondition}
+                onChange={this.handleChange}
+              />
+            </FormItem>
 
-        <Submit type="submit" value="Save" />
-      </Layout>
+            <Submit type="submit" value="Save" />
+          </Layout>
+        )}
+      </div>
     );
   }
 }
