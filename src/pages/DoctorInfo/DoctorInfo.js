33f14f0b7df98025775eac0f372dde8148330793
NbYb <sawazreal@gmail.com>
2020-12-18 14:31:56 +1100

    Doctors

--- src/pages/DoctorInfo/DoctorInfo.js
+++ src/pages/DoctorInfo/DoctorInfo.js
@@ -70,41 +70,12 @@ function B2F(doctorObject) {
 }
 
 
-// const doctors = {
-//   "0":{
-//     FirstName : "abc",
-//     SecondName : "edf",
-//     PhoneNumber : "15232133213",
-//     Email : "bcd@gmail.com",
-//     Language : "English",
-//     Specialization : "Dental",
-//   },
-//   "1":{
-//     FirstName : "bcd",
-//     SecondName : "123",
-//     PhoneNumber : "000000000",
-//     Email : "abc@gmail.com",
-//     Language : "Chinese",
-//     Specialization : "Dental",
-//   }
-// };
-
-
-
 class DoctorInfo extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      Doctor : {
-        // DoctorsID : this.props.match.DoctorsID,
-        // FirstName : doctors[this.props.match.params.DoctorsID].FirstName,
-        // SecondName : doctors[this.props.match.params.DoctorsID].SecondName,
-        // PhoneNumber : doctors[this.props.match.params.DoctorsID].PhoneNumber,
-        // Email : doctors[this.props.match.params.DoctorsID].Email,
-        // Language : doctors[this.props.match.params.DoctorsID].Language,
-        // Specialization : doctors[this.props.match.params.DoctorsID].Specialization,
-      }
+      Doctor : {}
     };
 
   }
@@ -152,15 +123,4 @@ class DoctorInfo extends React.Component {
     }
   }
 }
-// const DoctorInfo = ({match}) => {
-//     console.log(match);
-//     console.log(match.params.doctorsID);
-//     return (
-//       <Container>
-//         <Profile />
-//         <Contact />
-//         <Link />
-//       </Container>
-//     );
-//   };
 export default DoctorInfo;
