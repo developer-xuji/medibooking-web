e470df9bacbc094a78464667b117b05110ccaafe
Rmit-XuJi <xuji3595202@gmail.com>
2020-12-22 12:17:17 +1100

    file path motify

--- src/pages/FrontPage/FrontPage.js
+++ src/pages/FrontPage/FrontPage.js
@@ -2,13 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { HOME_BACKGROUND } from "../../constants";
 import { MOBILE_WIDTH } from "../../constants";
-import { JUMBOTRON_PIC } from '../../constants';
-import { getRoutePath } from "../../utils/GetRoute";
+import { JUMBOTRON_PIC } from "../../constants";
+import { getRoutePath } from "../../utils/getRoute";
 
 import Services from "./components/Services";
-import DataAds from "./components/DataAds"
-import DoctosIntro from "./components/DoctorsIntro"
-import CustomerResponse from "./components/CustomerResponse"
+import DataAds from "./components/DataAds";
+import DoctosIntro from "./components/DoctorsIntro";
+import CustomerResponse from "./components/CustomerResponse";
 
 const Layout = styled.div`
   display: flex;
@@ -24,8 +24,8 @@ const Layout = styled.div`
 `;
 
 const SloganContainer = styled.div`
-  width:100%;
-  height:500px;
+  width: 100%;
+  height: 500px;
   background-color: lightblue;
   border-bottom-right-radius: 30% 30px;
   border-bottom-left-radius: 30% 30px;
@@ -35,7 +35,7 @@ const SloganContainer = styled.div`
   background-position: center;
   padding: 32px 10%;
   color: #3f567c;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 
   hr {
     display: block;
@@ -52,7 +52,7 @@ const SloganContainer = styled.div`
     width: 50%;
     margin-top: 0;
     color: #5b7189;
-    font-family: 'Open Sans', sans-serif;
+    font-family: "Open Sans", sans-serif;
 
     @media screen and (max-width: ${MOBILE_WIDTH}) {
       font-size: 36px;
@@ -60,35 +60,39 @@ const SloganContainer = styled.div`
     }
   }
 
-    button {
-      border: none;
-      padding: 16px 32px;
-      background-color: #aecc54;
-      font-weight: bold;
-      border-radius: 8px;
-      a {
-        color: #fff;
-      }
+  button {
+    border: none;
+    padding: 16px 32px;
+    background-color: #aecc54;
+    font-weight: bold;
+    border-radius: 8px;
+    a {
+      color: #fff;
     }
+  }
 
-    button:hover {
-      background-color: transparent;
-      border: 1px solid #aecc54;
-      cursor: pointer;
-      a {
-        color: #aecc54;
-      }
+  button:hover {
+    background-color: transparent;
+    border: 1px solid #aecc54;
+    cursor: pointer;
+    a {
+      color: #aecc54;
     }
-
-`
+  }
+`;
 
 const FrontPage = () => {
   return (
     <Layout>
       <SloganContainer>
         <p>Welcome </p> <hr />
-        <h1>The very best <br /><b>general practitioners</b> for you and your family</h1>     
-        <button><a href={getRoutePath("booking")}>Booking Now</a></button>
+        <h1>
+          The very best <br />
+          <b>general practitioners</b> for you and your family
+        </h1>
+        <button>
+          <a href={getRoutePath("booking")}>Booking Now</a>
+        </button>
       </SloganContainer>
       <DataAds />
       <DoctosIntro />
--- src/pages/FrontPage/components/DoctorsIntro/index.js
+++ src/pages/FrontPage/components/DoctorsIntro/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-import Gallary from "./Gallary"
-import {THEME_COLOR} from "../../../../constants"
-import { getRoutePath } from "../../../../utils/GetRoute";
-
-
+import Gallary from "./Gallary";
+import { THEME_COLOR } from "../../../../constants";
+import { getRoutePath } from "../../../../utils/getRoute";
 
 const Layout = styled.div`
   width: 80%;
@@ -16,9 +14,8 @@ const Layout = styled.div`
 
   @media screen and (max-width: 880px) {
     text-align: center;
-}
-
-`
+  }
+`;
 const TextIntro = styled.div`
   padding: 0 36px;
   button {
@@ -33,13 +30,13 @@ const TextIntro = styled.div`
     }
 
     &:hover {
-        background-color: transparent;
-        border: 1px solid #aecc54;
-        cursor: pointer;
-        a {
-          color: #aecc54;
-        }
+      background-color: transparent;
+      border: 1px solid #aecc54;
+      cursor: pointer;
+      a {
+        color: #aecc54;
       }
+    }
   }
 
   hr {
@@ -51,45 +48,50 @@ const TextIntro = styled.div`
     width: 100px;
 
     @media screen and (max-width: 880px) {
-        margin: 8px auto;
+      margin: 8px auto;
     }
   }
-
-`
+`;
 
 const SubTitle = styled.div`
   color: #aecc54;
   font-weight: bold;
   font-size: 18px;
-  font-family: 'Open Sans', sans-serif;
-  `
+  font-family: "Open Sans", sans-serif;
+`;
 
 const Title = styled.div`
   color: ${THEME_COLOR};
   font-weight: bold;
   font-size: 36px;
-  font-family: 'Open Sans', sans-serif;
-`
+  font-family: "Open Sans", sans-serif;
+`;
 
 const Description = styled.div`
   font-size: 18px;
   line-height: 1.6;
   color: dimgray;
   margin: 18px 0;
-`
+`;
 
 const DoctorsIntro = () => {
-
-    return <Layout>
-        <Gallary></Gallary>
-        <TextIntro>
-            <SubTitle>Doctors</SubTitle>
-            <hr></hr>
-            <Title>Professional Expert</Title>
-            <Description>With over 170 Doctors, we’ll help find the right one for you and your family.</Description>
-            <button><a href={getRoutePath("doctors")}>View All  →</a></button>
-        </TextIntro>
+  return (
+    <Layout>
+      <Gallary></Gallary>
+      <TextIntro>
+        <SubTitle>Doctors</SubTitle>
+        <hr></hr>
+        <Title>Professional Expert</Title>
+        <Description>
+          With over 170 Doctors, we’ll help find the right one for you and your
+          family.
+        </Description>
+        <button>
+          <a href={getRoutePath("doctors")}>View All →</a>
+        </button>
+      </TextIntro>
     </Layout>
-}
+  );
+};
 
-export default DoctorsIntro
+export default DoctorsIntro;
