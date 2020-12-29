ef8245451984063cbfc50bd4a510274906798602
Rmit-XuJi <xuji3595202@gmail.com>
2020-12-29 11:57:43 +1100

    add getPatientById

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,19 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 import Container from "../Container";
-import Main from "./components/Main";
 import WithAuthenticationModalsAuthentication from "./components/Authentication";
 import NavItem from "./components/NavItem";
-import MobileNav from "./components/MobileNav";
 import { LOGO, THEME_COLOR } from "../../constants";
 import { getRoutePath } from "../../utils/getRoute";
 
 const Layout = styled.div`
   display: flex;
   display: -webkit-flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   line-height: 40px;
+  width: 70vw;
 `;
 
 const Wapper = styled.div`
@@ -24,6 +23,7 @@ const Wapper = styled.div`
   border-bottom: 1px solid #ccc;
   z-index: 1000;
   top: 0;
+  display: flex;
 `;
 
 const Logo = styled(NavItem)`
@@ -67,15 +67,12 @@ const Navigation = () => {
               <LogoDiv />
               <Title>MediBooking</Title>
             </Logo>
-
-            <Main />
           </Left>
           <Right>
             <WithAuthenticationModalsAuthentication>
               Authentication
             </WithAuthenticationModalsAuthentication>
           </Right>
-          <MobileNav />
         </Layout>
       </Container>
     </Wapper>
