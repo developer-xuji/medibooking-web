aa9358c2d582faab185f1a6f8966c87394983eae
matura1999 <yipengpeter@gmail.com>
2020-12-30 13:58:31 +1100

    make avatar S3 icon

--- src/components/Navigation/components/Authentication/components/Avatar/Avatar.js
+++ src/components/Navigation/components/Authentication/components/Avatar/Avatar.js
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 import { AVATAR_ICON } from "../../../../../../constants";
+const role = localStorage.getItem("ROLE");
+const doctorId = localStorage.getItem("USER_ID");
+const AVATAR_ICON_URL =
+  role && role === "ROLE_PATIENT"
+    ? AVATAR_ICON
+    : `https://medibookingbucket.s3-ap-southeast-2.amazonaws.com/DoctorsImage/IMG_${doctorId}.jpg`;
+console.log("doctorId", doctorId);
+
+// const { userId } = this.props;
 
 const Avatar = styled.div`
-  background: url(${AVATAR_ICON});
+  background: url(${AVATAR_ICON_URL});
   background-size: cover;
+  border-radius: 30px;
   height: 50px;
   width: 50px;
 `;
