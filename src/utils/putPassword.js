e41bad05550eb0fbbd3b23f43f639bcd09c79531
Rmit-XuJi <xuji3595202@gmail.com>
2020-12-30 16:57:17 +1100

    return put password data

--- src/utils/putPassword.js
+++ src/utils/putPassword.js
@@ -3,5 +3,5 @@ import authInstance from "../lib/authInstance";
 const putPassword = (accountId, account) =>
   authInstance
     .put("/register/" + accountId, account)
-    .then((response) => console.log(response.data));
+    .then((response) => response.data);
 export default putPassword;
